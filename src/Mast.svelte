8ef03601895c41786c0b0ad2b538be8c1e60c86c
LPMatrix <mubaraqsanusi908@gmail.com>
2020-03-13 11:29:27 +0100

    changed from carbon to bootstrap

--- src/Mast.svelte
+++ src/Mast.svelte
@@ -7,7 +7,7 @@
 	let response
 
 	const fetchLyrics = (term) => {
-		if (search_term.length == 0) {
+		if (term == null) {
 			error = true
 		} else {
 			let lyrics_data = {
@@ -22,11 +22,14 @@
 				response.then( res => {
 				response = res.data
 
-				const lyricURL = 'https://api.lyrics.ovh/v1/'+artist+'/'+song
+				for (var i = response.length - 1; i >= 0; i--) {
+					const lyricURL = 'https://api.lyrics.ovh/v1/'+response.artist.name+'/'+response.title
 
-				const lyric_res = fetch(lyricURL).then(result => {
+					const lyric_res = fetch(lyricURL).then(result => {
+						console.log(result.json())
+					})
+				}
 				
-			})
 				
 			})
 
@@ -43,41 +46,29 @@
 	}
 </script>
 
-<div class="bx--tile container">
+<div class="container">
 	{#if error}
 		<Notification />
 	{/if}
-	<form on:submit = {submit} >
-	  <div data-search role="search" class="bx--search bx--search--lg">
-	    <label id="search-input-label-1" class="bx--label" for="search__input-1">Search</label>
-	    <input class="bx--search-input" type="text" id="search__input-1" placeholder="MInd is a Prison" bind:value={search_term}>
-	    <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--search-magnifier" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true"><path d="M15,14.3L10.7,10c1.9-2.3,1.6-5.8-0.7-7.7S4.2,0.7,2.3,3S0.7,8.8,3,10.7c2,1.7,5,1.7,7,0l4.3,4.3L15,14.3z M2,6.5	C2,4,4,2,6.5,2S11,4,11,6.5S9,11,6.5,11S2,9,2,6.5z"></path></svg>
-	    <button class="bx--search-close bx--search-close--hidden" title="Clear search
-	        input" aria-label="Clear search input">
-	      
-	      <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--search-clear" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true"><path d="M12 4.7L11.3 4 8 7.3 4.7 4 4 4.7 7.3 8 4 11.3 4.7 12 8 8.7 11.3 12 12 11.3 8.7 8z"></path></svg>
-	      
-	    </button>
+
+	<form class="form-inline" on:submit = {submit}>
+	  <div class="form-group mx-sm-3 mb-2 col-sm-9">
+	    <input type="text" class="form-control col-sm-12" id="inputPassword2" placeholder="Mind is a prison" bind:value={search_term}>
 	  </div>
-	  <div class="example-button-wrapper center">
-	    <button class=" bx--btn bx--btn--primary bx--btn--field"  type="submit">
-	      Search
-	    </button>
-	  </div>
-  </form>
-</div>
+	  <button type="submit" class="btn btn-primary mb-2">Search</button>
+	</form>
+
+
 {#if response == undefined} <h3>No lyrics available</h3>
 {:else}
-<div class="bx--tile">
-<ul data-accordion class="bx--accordion">
+<div class="accordion" id="accordionExample">
 	{#each response as lyric, key}
-		<SearchArea cover = {lyric.album.cover} title = {lyric.title} key = {key}/>
+		<SearchArea cover = {lyric.album.cover} title = {lyric.title} key = {key} lyrics = {lyric.title}/>
 	{/each}
-</ul>
 </div>
 
 {/if}
-
+</div>
 <style type="text/css">
 	.container{
 		margin-top: 5em;
--- src/SearchArea.svelte
+++ src/SearchArea.svelte
@@ -1,35 +1,29 @@
 <script type="text/javascript">
 	export let cover = ""
 	export let title = ""
+	export let lyrics = ""
 	export let key
 </script>
 
-<li data-accordion-item class="bx--accordion__item">
-  <button class="bx--accordion__heading" aria-expanded="false" aria-controls="pane{key}">
-    <svg focusable="false" preserveAspectRatio="xMidYMid meet" style="will-change: transform;" xmlns="http://www.w3.org/2000/svg" class="bx--accordion__arrow" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true"><path d="M11 8L6 13 5.3 12.3 9.6 8 5.3 3.7 6 3z"></path></svg>
-    <div class="bx--accordion__title"><img src="{cover}" alt="Cover image"><h3>{title}</h3></div>
-  </button>
-  <div id="pane{key}" class="bx--accordion__content">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+      	<img src="{cover}" alt="Cover image">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#pane{key}" aria-expanded="true" aria-controls="collapseOne">
+          {title}
+        </button>
+      </h2>
+    </div>
+
+    <div id="pane{key}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        {lyrics}
+      </div>
+    </div>
   </div>
-</li>
 
 <style type="text/css">
-	.bx--tile{
-		margin-top: 1em;
-	}
-	.bx--accordion__content{
-		text-align: center;
-	}
-	h3{
-		line-height: 1em;
-    	padding-left: 1em;
-    	text-align: center;
-    	margin: auto;
-    	margin-left: 1em;
-	}
-	.bx--accordion__title{
-		display: flex;
+	img{
+		height: 3em;
 	}
 </style>
